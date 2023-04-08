b69629f6e902fba241c0d16e4401df16bb5ce038
Na Lin <lna483018@gmail.com>
2023-04-08 15:57:52 -0400

    reset server/index.js to root directory

---
+++ index.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+const colors = require("colors");
+const { db } = require("./server/db");
+const PORT = process.env.PORT || 8080;
+const app = require("./server/app");
+const seed = require("./script/seed");
+
+const init = async () => {
+  try {
+    if (process.env.SEED === "true") {
+      await seed();
+    } else {
+      await db.sync();
+    }
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+init();
+// start listening (and create a 'server' object representing our server)
+const server = app.listen(PORT, () =>
+  console.log(`Mixing it up on port ${PORT}`)
+);
+// const io = require("socket.io")(server);
+// require("./socket")(io);
--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./.env" });
-const colors = require("colors");
-const { db } = require("./db");
-const PORT = process.env.PORT || 8080;
-const app = require("./app");
-const seed = require("../script/seed");
+// const dotenv = require("dotenv");
+// dotenv.config({ path: "./.env" });
+// const colors = require("colors");
+// const { db } = require("./db");
+// const PORT = process.env.PORT || 8080;
+// const app = require("./app");
+// const seed = require("../script/seed");
 
-const init = async () => {
-  try {
-    if (process.env.SEED === "true") {
-      await seed();
-    } else {
-      await db.sync();
-    }
-  } catch (ex) {
-    console.log(ex);
-  }
-};
+// const init = async () => {
+//   try {
+//     if (process.env.SEED === "true") {
+//       await seed();
+//     } else {
+//       await db.sync();
+//     }
+//   } catch (ex) {
+//     console.log(ex);
+//   }
+// };
 
-init();
-// start listening (and create a 'server' object representing our server)
-const server = app.listen(PORT, () =>
-  console.log(`Mixing it up on port ${PORT}`)
-);
-// const io = require("socket.io")(server);
-// require("./socket")(io);
+// init();
+// // start listening (and create a 'server' object representing our server)
+// const server = app.listen(PORT, () =>
+//   console.log(`Mixing it up on port ${PORT}`)
+// );
+// // const io = require("socket.io")(server);
+// // require("./socket")(io);
