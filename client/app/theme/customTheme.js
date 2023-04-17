d4f7ccb9b4798f072a8a5a760af3303b3620978b
Na Lin <lna483018@gmail.com>
2023-04-16 21:31:20 -0400

    add custom theme

---
+++ client/app/theme/customTheme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+export const customThemeOptions = createTheme({
+  palette: {
+    // primary:orange 500/200/700
+    primary: {
+      main: "#06b6d4",
+      light: "#a5f3fc",
+      dark: "#0e7490",
+    },
+    // secondary: cyan 500/200/700
+    success: {
+      main: "#f97316",
+      light: "#fed7aa",
+      dark: "#c2410c",
+    },
+  },
+});
+
+// primar:
